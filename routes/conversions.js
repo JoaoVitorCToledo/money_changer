b08d9e7396f1c28117797b377294521432523945
João Vitor Custodio Toledo <joaovitorc.toledo@gmail.com>
2023-02-13 16:14:42 -0300

    Adds conversion request post route

--- routes/conversions.js
+++ routes/conversions.js
@@ -3,10 +3,17 @@ const { ConversionRequest } = require("../models/conversionRequest");
 var router = express.Router();
 
 
-/* GET users listing. */
-router.get('/', async function(req, res, next) {
+/* GET conversion request listing. */
+router.get('/', async function(req, res) {
   const allRequests = await ConversionRequest.find();
   res.status(200).json(allRequests);
+});
+
+/* POST creates conversion */
+router.post("/", async (req, res) => {
+  const newConversionRequest = new ConversionRequest({ ...req.body });
+  const insertedConversionRequest = await newConversionRequest.save();
+  return res.status(201).json(insertedConversionRequest);
 });
 
 module.exports = router;
