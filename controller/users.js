c19f1cdc2bb880e1953909f2ce37a766df3b4880
João Vitor Custodio Toledo <joaovitorc.toledo@gmail.com>
2023-02-16 22:51:47 -0300

    Add swagger to project

--- controller/users.js
+++ controller/users.js
@@ -1,7 +1,6 @@
 const { ConversionRequest } = require('../models/conversionRequest')
 
 const getUserConverions = async function (req, res) {
-  console.log('aaaaaaaaaaaaaaaaaaaaaaaa')
   const { id } = req.params
   const allRequests = await ConversionRequest.find({ user_id: id })
   res.status(200).json(allRequests)
